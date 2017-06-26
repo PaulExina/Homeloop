a36ac88b0788e509435a925b7e2192254286ad20
PaulExina <paulexina26@gmail.com>
2017-06-26 22:42:31 +0200

    Debug #1

--- homeloop/db/seeds.rb
+++ homeloop/db/seeds.rb
@@ -12,16 +12,16 @@ transactions_array = transactions_examples.split("\n");
 
 transactions_array.drop(1).each do |tr|
 
-    tr_array = tr.split(",")²   
+    tr_array = tr.split(",")  
 
     if (Transaction.find_by(trans_id: tr_array[0].to_i) == nil)
         Transaction.create(:trans_id => tr_array[0].to_i, 
                        :rooms => tr_array[1].to_i,
                        :area => tr_array[2].to_i,
-                       :price => tr_array[3].to_i,
+                       :price => tr_array[3].to_f,
                        :lat => tr_array[4].to_f,
                        :lon => tr_array[5].to_f,
                        :date => tr_array[6].to_date)
-
     end
+
 end
